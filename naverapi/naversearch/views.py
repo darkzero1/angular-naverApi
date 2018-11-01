20934f33f665233c8d3ce28172bbab8055eec434
ksks0908 <ksks0908@naver.com>
2018-11-01 17:09:47 +0900

    네이버 검색 api

--- naverapi/naversearch/views.py
+++ naverapi/naversearch/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
-
+import json
+import urllib.request
 # Create your views here.
-def naverBookSearchApi():
+def naverBookSearchApi(request):
 	#해당 id secretkey는 git에 넣지않음
-	client_id = ""
-	client_secret = ""
+	client_id = "sqj46vL3xz4UmCdh5cn3"
+	client_secret = "uG9d8QswBN"
 	encText = urllib.parse.quote("검색할 단어")
 	url = "https://openapi.naver.com/v1/search/blog?query=" + encText # json 결과
 	# url = "https://openapi.naver.com/v1/search/blog.xml?query=" + encText # xml 결과
